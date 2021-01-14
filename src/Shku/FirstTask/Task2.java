0d9ca1f1c76970b99b8505d9d66e6de79ac0f6f6
ICreActive <negumadness@gmail.com>
2021-01-15 01:45:56 +0300

    Доработан интерфейс

--- src/Shku/FirstTask/Task2.java
+++ src/Shku/FirstTask/Task2.java
@@ -1,22 +1,36 @@
 package Shku.FirstTask;
 
+import com.sun.source.tree.IfTree;
+
+import javax.swing.*;
+
 public class Task2 {
     public static void main(String[] args) {
 
-        int Chislo1;
-        int Chislo2;
-        int VvediteChislo;
-        VvediteChislo = 1980;
-        Chislo1 = 3;
-        Chislo2 = 9;
-        if (VvediteChislo<Chislo1){
-            System.out.println("Ваше число относится к первому диапазону");
-        }
-        if (VvediteChislo>Chislo1 && VvediteChislo<Chislo2){
-            System.out.println("Ваше число относится ко второму диапазону");
-        }
-        if (VvediteChislo>Chislo2){
-            System.out.println("Ваше число относится к третьему диапазону");
-        }
+        double Number1;
+        double Number2;
+        double Choice;
+        String UserChoice;
+
+        do {
+
+            UserChoice = JOptionPane.showInputDialog("Введите любое число");
+            if (!UserChoice.matches("[0-9]+")) continue;
+            Choice = Double.parseDouble(UserChoice);
+            Number1 = 1;
+            Number2 = 100;
+            if (Choice < Number1) {
+                System.out.println("Ваше число относится к первому диапазону");
+            } else if (Choice > Number1 && Choice < Number2) {
+                System.out.println("Ваше число относится ко второму диапазону");
+            }
+            else if (Choice > Number2) {
+                System.out.println("Ваше число относится к третьему диапазону");
+            }
+            else  {
+                System.out.println("Число находится на границе диапозонов");
+            }
+        } while (true);
+
     }
 }
