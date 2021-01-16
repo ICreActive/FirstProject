2d8e4ed041188b8ebfdf1dbe59b6d56b0e63667d
ICreActive <negumadness@gmail.com>
2021-01-16 22:46:37 +0300

    - Fixed calculation algorithm - Fixed bug with "0"

--- src/Shku/FirstTask/Task4.java
+++ src/Shku/FirstTask/Task4.java
@@ -1,34 +1,52 @@
 package Shku.FirstTask;
 
+import java.util.Scanner;
+
 public class Task4 {
+    private static final int c = 10;
+
     public static void main(String[] args) {
-        int a = 0;
-        float b = 4;
-        int c = 10;
-        float d;
 
-        if (a > b && b!=0) {
-
-            d = (a / b * 100) - 100;
-            System.out.println(d + "%");
-            if (d > c) {
-                System.out.println("Значения не близки");
-            } else
-                System.out.println("Значения близки");
-        }
-        else {
-            d = (byte) ((b / a * 100) - 100);
-            if (a!=0) {
-
-                System.out.println(d + "%");
-                if (d > c) {
-                    System.out.println("Значения не близки");
-                } else {
-                    System.out.println("Значения близки");
-                }
-            } else {
-                System.out.println("На Ноль делить нельзя");
+        float a;
+        Scanner sc1 = new Scanner(System.in);
+        System.out.println("Введите 1-е число (любое кроме 0)");
+        if (sc1.hasNextFloat()) {
+            a = sc1.nextFloat();
+            if (a == 0) {
+                System.out.println("Для данного числа задача невыполнима");
+                return;
             }
+        } else {
+            System.out.println("Вы ввели некорректные данные");
+            return;
+        }
+
+        float b;
+        Scanner sc2 = new Scanner(System.in);
+        System.out.println("Введите 2-е число (любое кроме 0)");
+        if (sc2.hasNextFloat()) {
+            b = sc2.nextFloat();
+            sc1.close();
+            sc2.close();
+            if (a == 0) {
+                System.out.println("Для данного числа задача невыполнима");
+                return;
+            }
+        } else {
+            System.out.println("Вы ввели некорректные данные");
+            return;
+        }
+
+        float lowValue = (a - a * c / 100); // low value
+        float highValue = (a + a * c / 100); // High value
+
+        System.out.println("Диапозон близких значений от " + lowValue + " до " + highValue);
+
+        if (b <= highValue && b >= lowValue) {
+            System.out.println("Значения близки");
+        } else {
+
+            System.out.println("Значения не близки");
         }
     }
 }
