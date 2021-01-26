f1966e32a57f878ae67b14f707ba848d9e52e181
ICreActive <negumadness@gmail.com>
2021-01-26 17:14:54 +0300

    Added solution for task 2

--- src/Shku/SecondTask/Task2.java
+++ src/Shku/SecondTask/Task2.java
@@ -6,29 +6,46 @@ public class Task2 {
 
     public static void main(String[] args) {
 
-        // Add Array for days
-
-        int[] days = new int[31];
-        days[0] = 0;
-        for (int i = 1; i < days.length; i++) {
-            days[i] = days[i - 1] + 1;
-        }
-
-        //Add Array for Month
-
-        int[] Month = new int[13];
-        Month[0] = 0;
-        for (int j = 1; j < Month.length; j++) {
-            Month[j] = Month[j - 1] + 1;
-        }
-
         int inputMonth;
         System.out.println("Введите номер месяца");
         Scanner sc = new Scanner(System.in);
         inputMonth = sc.nextInt();
+        int a;
 
-        for (int j = 0; j < Month.length; j++) {
-            if (inputMonth == Month[j]) {
+        if (inputMonth == 1 ||
+                inputMonth == 3 ||
+                inputMonth == 5 ||
+                inputMonth == 7 ||
+                inputMonth == 8 ||
+                inputMonth == 10 ||
+                inputMonth == 12) {
+
+            a = 31;
+        } else if (inputMonth == 2) {
+            a = 28;
+        } else {
+            a = 30;
+        }
+
+        // Add Array for days
+
+        int[] days = new int[a];
+        days[0] = 1;
+        for (int i = 1; i < days.length; i++) {
+            days[i] = days[i - 1] + 1;
+        }
+
+        //Add Array for month
+
+        int[] month = new int[13];
+        month[0] = 0;
+        for (int j = 1; j < month.length; j++) {
+            month[j] = month[j - 1] + 1;
+        }
+
+
+        for (int j = 0; j < month.length; j++) {
+            if (inputMonth == month[j]) {
                 int inputDay;
                 System.out.println("Введите номер дня в месяце");
                 Scanner sc1 = new Scanner(System.in);
@@ -45,13 +62,13 @@ public class Task2 {
                     }
                 }
                 System.out.println("Данного числа нет в этом месяце");
+                return;
 
             } else {
-                Month[j]++;
+                month[j]++;
             }
         }
         System.out.println("Не существует такого месяца");
-
 
     }
 
