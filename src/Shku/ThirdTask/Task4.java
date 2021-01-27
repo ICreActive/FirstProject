e1dfdd3d7df7f2add333fcbb7b71eea915a6f40f
ICreActive <negumadness@gmail.com>
2021-01-27 23:17:26 +0300

    Corrected solution algorithm Task4

--- src/Shku/ThirdTask/Task4.java
+++ src/Shku/ThirdTask/Task4.java
@@ -1,31 +1,30 @@
 package Shku.ThirdTask;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 public class Task4 {
 
     public static void main(String[] args) {
 
         String str = "I become so numb I can't feel you there Become so tired So much more aware";
 
+
         String[] words = str.split(" ");
         for (String word : words) {
 
             word = word.toLowerCase();
-            str = str.toLowerCase();
-            Pattern pattern = Pattern.compile(word);
-            Matcher matcher = pattern.matcher(str);
             int count = 0;
-            while (matcher.find()) {
-                count++;
 
+            for (int i = 0; i < words.length; i++) {
+                if (words[i].equalsIgnoreCase(word)) {
+                    count++;
+                }
             }
-            System.out.println(word + "  - " + count);
+            System.out.println(word + " -  " + count);
         }
-
     }
 }
+
+
+
 
 
 
