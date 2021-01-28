98e8690a32f200379e3e466325d33c974444564f
ICreActive <negumadness@gmail.com>
2021-01-28 12:54:02 +0300

    Added solution for task 1

---
+++ src/Shku/ThirdTask/Task1.java
@@ -0,0 +1,38 @@
+package Shku.ThirdTask;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class Task1 {
+
+    public static void main(String[] args) {
+
+        // method String
+        int[] array = new int[100];
+
+        for (int i = 0; i < array.length; i++) {
+            Random rd = new Random();
+            int rdNumber = rd.nextInt(1000);
+            array[i] = rdNumber;
+
+        }
+
+        String str = Arrays.toString(array);
+        str = str.replaceAll("^\\[|\\]$|, ", " ");
+        System.out.print("method String: " + str);
+        System.out.println();
+
+        // method StringBuilder
+        int[] array1 = new int[100];
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < array1.length; i++) {
+            Random rd1 = new Random();
+            int rdNumber1 = rd1.nextInt(1000);
+            sb.append(rdNumber1).append(" ");
+        }
+        System.out.println("method StringBuilder: " + sb);
+    }
+
+
+}
+
